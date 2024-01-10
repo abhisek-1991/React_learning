8dbf76ae7114c89412f7c6118a4428b876880ba4
user <abhisek.may1991@gmail.com>
2024-01-10 19:29:10 +0530

    useState1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
- 
+// Moved the Todo component outside of DataComponent
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+     
+    </div>
+  );
 }
 
-export default App
+function DataComponent() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos/')
+      .then(function (res) {
+        setTodos(res.data); // Use res.data directly
+      })
+      .catch(function (error) {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {todos.map(todo => (
+        console.log(todos),
+        <Todo key={todo.id} title={todo.title}  />
+        // Use todo.body instead of todo.description for the description
+      ))}
+    </div>
+  );
+}
+
+export default DataComponent;
