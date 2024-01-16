fc6aa3cac93a6fc1e95e6937210e0e3da2443e19
user <abhisek.may1991@gmail.com>
2024-01-16 13:38:01 +0530

    client-side routing and topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,24 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { Dashboard } from "./components/Dashboard";
+import { Landing } from "./components/Landing";
 
-const ColorPalette = ({ colors }) => (
-  <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-    {colors.map((color, index) => (
-      <div
-        key={index}
-        style={{
-          width: '50px',
-          height: '50px',
-          backgroundColor: color,
-          margin: '5px',
-        }}
-      />
-    ))}
-  </div>
-);
-
-const ColorPaletteGenerator = () => {
-  const [colorCount, setColorCount] = useState(5);
-
-  const generateRandomColor = useCallback(() => {
-    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    return randomColor;
-  }, []);
-
-  const generateColorPalette = useCallback(() => {
-    const palette = Array.from({ length: colorCount }, generateRandomColor);
-    console.log(palette);
-    return palette;
-  }, [colorCount, generateRandomColor]);
-  console.log(generateColorPalette());
-  const memoizedColorPalette = useMemo(() => generateColorPalette(), [generateColorPalette]);
-
+function App() {
   return (
     <div>
-      <h2>Color Palette Generator</h2>
-      <label>
-        Number of Colors:
-        <input
-          type="number"
-          min="1"
-          max="100"
-          value={colorCount}
-          onChange={(e) => setColorCount(Number(e.target.value))}
-        />
-      </label>
-      <ColorPalette colors={memoizedColorPalette} />
-    </div>
-  );
-};
+      <div style={({background: "black"})}>
+        Hi this is the topbar
+      </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/" element={<Landing/>}/>
+      </Routes>
 
-export default ColorPaletteGenerator;
+      </BrowserRouter>
+      </div>
+  )
+}
+
+
+export default App;
+
---
+++ src/progress.md
@@ -0,0 +1,9 @@
+7.1
+routing,prop drilling,context api
+
+Jargon
+single page applications
+client side bundle
+client side routing
+
+1.16.26
